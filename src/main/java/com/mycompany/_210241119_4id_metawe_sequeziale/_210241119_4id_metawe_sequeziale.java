9df27dda1494eeae4a2ab62122a5585995ea09dd
ospite <ospite@PC11-LIM>
2024-11-19 11:55:43 +0100

    prova

---
+++ src/main/java/com/mycompany/_210241119_4id_metawe_sequeziale/_210241119_4id_metawe_sequeziale.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany._210241119_4id_metawe_sequeziale;
+
+/**
+ *
+ * @author ospite
+ */
+public class _210241119_4id_metawe_sequeziale {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
+public static void main(String[] args) {
+Minuscolo min = new Minuscolo();
+        min.Leggi();
+Scanner in = new Scanner(System.in);
